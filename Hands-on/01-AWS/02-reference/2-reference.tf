9e19d54d622371ff697afef9e80385ca5d6239c7
atingupta2005 <atingupta2005@gmail.com>
2025-07-01 16:02:47 +0530

    Update 2-reference.tf

--- Hands-on/01-AWS/02-reference/2-reference.tf
+++ Hands-on/01-AWS/02-reference/2-reference.tf
@@ -1,25 +1,28 @@
-provider "aws" {
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
 }
 
+provider "aws" {
 
-# A resource block declares a resource of a given type ("aws_instance") 
-# with a given local name ("myec2"). 
-# The name is used to refer to this resource from elsewhere in the same Terraform module, 
-# but has no significance outside that module's scope.
+}
+
 resource "aws_instance" "myec2" {
-   ami = "ami-082b5a644766e0e6f"
-   instance_type = "t2.micro"
+  ami           = "ami-03598bf9d15814511"
+  instance_type = "t2.micro"
 }
 
 resource "aws_eip" "lb" {
-  vpc      = true
 }
 
 resource "aws_eip_association" "eip_assoc" {
   instance_id   = aws_instance.myec2.id
   allocation_id = aws_eip.lb.id
 }
-
 
 resource "aws_security_group" "allow_tls" {
   name        = "att-security-group"
@@ -29,7 +32,14 @@ resource "aws_security_group" "allow_tls" {
     to_port     = 443
     protocol    = "tcp"
     cidr_blocks = ["${aws_eip.lb.public_ip}/32"]
-
-#    cidr_blocks = [aws_eip.lb.public_ip/32]
   }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  depends_on = [aws_eip.lb]
 }
